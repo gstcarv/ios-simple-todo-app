432e3d516c9a8c629a9474b9d0f95cf0800653b4
Gustavo Carvalho <gstcarv@gmail.com>
2022-04-06 16:26:45 -0300

    Initial Commit

---
+++ UserDefaultsTodoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserDefaultsTodoList
+//
+//  Created by Gustavo Carvalho on 06/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
