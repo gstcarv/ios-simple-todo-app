bce6dffb61ef963b4deb2397941e904819d12a0a
Gustavo Carvalho <gstcarv@gmail.com>
2022-04-06 17:27:57 -0300

    feat: base todo and new todo layout

--- UserDefaultsTodoList/Controllers/TodoListTableViewController.swift
+++ UserDefaultsTodoList/Controllers/TodoListTableViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TodoListTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
